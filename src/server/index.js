a8ffc51d0c053481cd8ffae9e29b8fef4a704ac0
gabeyamartino <gabeyamartino@gmail.com>
2024-02-21 12:38:53 -0500

    Changed ports for self-hosting

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const getShowsFromYear = require("./controllers/getShowsFromYear");
 const router = require("../lib/router");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
